9f965e5e24017874213a694eaee4ebd7d7927039
Murat Akbal <murat@carbonhealth.com>
2018-07-03 10:40:57 -0700

    publish

--- dist/src/utils/computeProps.js
+++ dist/src/utils/computeProps.js
@@ -1,7 +1,5 @@
 "use_strict";
-var _lodash=require("lodash");var _lodash2=_interopRequireDefault(_lodash);
-var _ReactNativePropRegistry=require("react-native/Libraries/Renderer/shims/ReactNativePropRegistry");var _ReactNativePropRegistry2=_interopRequireDefault(_ReactNativePropRegistry);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}
-
+var _lodash=require("lodash");var _lodash2=_interopRequireDefault(_lodash);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}
 
 module.exports=function(incomingProps,defaultProps){
 
@@ -25,14 +23,8 @@ var computedPropsStyle={};
 computedProps.style={};
 if(Array.isArray(incomingPropsStyle)){
 _lodash2.default.forEach(incomingPropsStyle,function(style){
-if(typeof style==="number"){
-_lodash2.default.merge(computedPropsStyle,_ReactNativePropRegistry2.default.getByID(style));
-}else{
 _lodash2.default.merge(computedPropsStyle,style);
-}
 });
-}else if(typeof incomingPropsStyle==="number"){
-computedPropsStyle=_ReactNativePropRegistry2.default.getByID(incomingPropsStyle);
 }else{
 computedPropsStyle=incomingPropsStyle;
 }
