02a5715a689e6d22e8188450a0d0e09f6bc67a57
Murat Akbal <murat@carbonhealth.com>
2018-07-03 10:17:12 -0700

    Remove ReactNativePropRegistry dependency

--- src/utils/computeProps.js
+++ src/utils/computeProps.js
@@ -1,8 +1,6 @@
 "use_strict";
 import _ from "lodash";
-import ReactNativePropRegistry from "react-native/Libraries/Renderer/shims/ReactNativePropRegistry";
-// For compatibility with RN 0.25
-// import ReactNativePropRegistry from "react-native/Libraries/ReactNative/ReactNativePropRegistry";
+
 module.exports = function(incomingProps, defaultProps) {
   // External props has a higher precedence
   let computedProps = {};
@@ -25,14 +23,8 @@ module.exports = function(incomingProps, defaultProps) {
     computedProps.style = {};
     if (Array.isArray(incomingPropsStyle)) {
       _.forEach(incomingPropsStyle, style => {
-        if (typeof style === "number") {
-          _.merge(computedPropsStyle, ReactNativePropRegistry.getByID(style));
-        } else {
           _.merge(computedPropsStyle, style);
-        }
       });
-    } else if (typeof incomingPropsStyle === "number") {
-      computedPropsStyle = ReactNativePropRegistry.getByID(incomingPropsStyle);
     } else {
       computedPropsStyle = incomingPropsStyle;
     }
